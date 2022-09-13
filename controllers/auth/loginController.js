a0ea9c6575f853aab01db8e3997fe6dd09a59ef5
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-13 17:22:22 +0600

    refresh token complete

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -1,8 +1,9 @@
 import Joi from 'joi';
-import { User } from '../../models';
+import { User,RefreshToken } from '../../models';
 import CustomErrorHandler from '../../services/CustomErrorHandler';
 import bcrypt from 'bcrypt'
 import JwtService from '../../services/JwtService';
+import { REFRESH_SECRET } from '../../config';
 
 const loginController = {
   async login(req,res,next){
@@ -31,7 +32,10 @@ const loginController = {
       }
 
       const access_token = JwtService.sign({_id:user._id,role:user.role});
-      res.json({access_token:access_token});
+      const refresh_token = JwtService.sign({_id:user._id,role:user.role},'1y',REFRESH_SECRET);
+
+      await RefreshToken.create({token:refresh_token});
+      res.json({access_token,refresh_token});
     }catch(err){
       return next(err);
     }
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 export {default as registerController} from './auth/registerController';
 export {default as loginController} from './auth/loginController';
 export {default as userControler} from './auth/userController';
+export {default as refreshControler} from './auth/refreshController';
