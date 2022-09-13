1f943309f5a3e4c6eb20300f2e8d3637740b0b7e
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-13 19:34:55 +0600

    delete product

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/me',auth,userController.me)
 router.post('/refresh',refreshController.refresh)
 router.post('/logout',auth,loginController.logout)
 
-router.post ('/products',[auth,admin],productController.store)
-router.put (`/products/:id`,[auth,admin],productController.update)
+router.post('/products',[auth,admin],productController.store)
+router.put(`/products/:id`,[auth,admin],productController.update)
+router.delete(`/products/:id`,[auth,admin],productController.destroy)
 
 export default router;
